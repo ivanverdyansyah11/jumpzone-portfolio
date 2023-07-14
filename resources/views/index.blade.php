43c28ab4694cba70425e01ec707e5a5e28b3fdc7
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-14 19:36:19 +0800

    Progress: Finish Footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
 
 </head>
 
-<body id="home" style="padding-bottom: 100px">
+<body id="home">
 
     {{-- NAVBAR --}}
     <nav class="navbar navbar-expand-lg py-3 position-relative">
@@ -40,7 +40,6 @@
 
     <main>
         <div class="container">
-
             {{-- HERO SECTION --}}
             <section class="hero d-flex align-items-center py-4 py-lg-0 pb-xl-5 pb-xxl-0" id="hero">
                 <div class="row align-items-center">
@@ -965,8 +964,71 @@
                 </div>
             </section>
             {{-- END FAQ SECTION --}}
-
         </div>
+        {{-- FOOTER --}}
+        <footer class="footer" id="footer">
+            <div class="container">
+                <div class="row" style="margin-bottom: 100px">
+                    <div class="col-6 col-md-3 mb-5 mb-md-0">
+                        <a class="footer-brand d-inline-block" href="#home" style="margin-bottom: 18px;">
+                            <img src="{{ asset('assets/img/logo/logo-brand-reverse.svg') }}" class="img-fluid w-100"
+                                alt="Logo Brand">
+                        </a>
+                        <p class="paragraph-small" style="margin-bottom: 30px;">Welcome to JumpZone, the ultimate
+                            destination for trampoline
+                            enthusiasts of all ages.</p>
+                        <div class="wrapper-sosmed d-flex gap-2">
+                            <a href="#" class="sosmed-icon d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('assets/img/icon/facebook-icon.svg') }}" class="img-fluid"
+                                    alt="Facebook Icon" width="16">
+                            </a>
+                            <a href="#" class="sosmed-icon d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('assets/img/icon/whatsapp-icon.svg') }}" class="img-fluid"
+                                    alt="Whatsapp Icon" width="16">
+                            </a>
+                            <a href="#" class="sosmed-icon d-flex justify-content-center align-items-center">
+                                <img src="{{ asset('assets/img/icon/twitter-icon.svg') }}" class="img-fluid"
+                                    alt="Twitter Icon" width="16">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="offset-lg-2 offset-xl-3 col-6 col-md-3 col-lg-2 mb-5 mb-md-0">
+                        <h6 style="margin-bottom: 20px">Quick Links</h6>
+                        <div class="wrapper d-flex flex-column gap-3">
+                            <a href="#home" class="paragraph-small">Home</a>
+                            <a href="#about" class="paragraph-small">About Us</a>
+                            <a href="#guideline" class="paragraph-small">Safety Guidelines</a>
+                            <a href="#pricing" class="paragraph-small">Pricing Packages</a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-3 col-xl-2">
+                        <h6 style="margin-bottom: 20px">Our Adventages</h6>
+                        <div class="wrapper d-flex flex-column gap-3">
+                            <a href="#adventage" class="paragraph-small">Active Fun for All Ages</a>
+                            <a href="#adventage" class="paragraph-small">Safety First</a>
+                            <a href="#adventage" class="paragraph-small">Variety of Attractions</a>
+                            <a href="#adventage" class="paragraph-small">Fitness and Fun Combined</a>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-3 col-lg-2">
+                        <h6 style="margin-bottom: 20px">Help & guide</h6>
+                        <div class="wrapper d-flex flex-column gap-3">
+                            <a href="" class="paragraph-small">Terms & Conditions</a>
+                            <a href="" class="paragraph-small">Privacy Policy</a>
+                            <a href="" class="paragraph-small">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12 copyright text-center">
+                        <p>Copyright © 2023 JumpZone. All Right Reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        {{-- END FOOTER --}}
     </main>
 
     {{-- SCRIPT JS --}}
