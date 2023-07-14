a991ffbd668c904807867c1d47b640c41170ae97
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-14 08:35:17 +0800

    Progress: Connect Link Style CSS & Script JS

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>JumpZone | Trampoline Zone</title>
+
+    {{-- STYLE CSS --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+</head>
+
+<body>
+
+    <p>Hello World</p>
+
+    {{-- SCRIPT JS --}}
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+</body>
+
+</html>
