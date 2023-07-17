0081b12b385319259b10c0bbb3f2a887fc5a5c01
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-07-17 11:29:41 +0800

    Progress: Add New Modal for Adventage Section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -104,11 +104,36 @@
                             <p class="paragraph-small" style="margin-bottom: 16px;">JumpZone offers an inclusive and
                                 exhilarating experience for
                                 jumpers of all ages.</p>
-                            <button class="card-link d-flex align-items-center gap-2 p-0">
+                            <button class="card-link d-flex align-items-center gap-2 p-0" data-bs-toggle="modal"
+                                data-bs-target="#adventageModal1">
                                 More Detail
                                 <img src="{{ asset('assets/img/icon/link-arrow-icon.svg') }}" class="img-fluid"
                                     alt="Link Arrow Icon">
                             </button>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="adventageModal1" tabindex="-1" aria-labelledby="adventageModal1Label"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="pricing-price">Active Fun for All Ages</h4>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="paragraph-small">JumpZone offers an inclusive and exhilarating experience
+                                        for jumpers of all ages. From kids to adults, everyone can enjoy the thrill of
+                                        bouncing, flipping, and soaring through the air in a safe and controlled
+                                        environment.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button"
+                                        class="button-reverse-small d-flex align-items-center gap-2"
+                                        data-bs-dismiss="modal">Close Modal</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -122,11 +147,37 @@
                             <h6 style="margin-bottom: 4px;">Safety First</h6>
                             <p class="paragraph-small" style="margin-bottom: 16px;">We prioritize the safety of our
                                 jumpers. Our trampolines are carefully maintained.</p>
-                            <button class="card-link d-flex align-items-center gap-2 p-0">
+                            <button class="card-link d-flex align-items-center gap-2 p-0" data-bs-toggle="modal"
+                                data-bs-target="#adventageModal2">
                                 More Detail
                                 <img src="{{ asset('assets/img/icon/link-arrow-icon.svg') }}" class="img-fluid"
                                     alt="Link Arrow Icon">
                             </button>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="adventageModal2" tabindex="-1"
+                        aria-labelledby="adventageModal2Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="pricing-price">Safety First</h4>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="paragraph-small">At JumpZone, we prioritize the safety of our jumpers.
+                                        Our trampolines are carefully maintained, and we enforce strict safety
+                                        guidelines to ensure a secure and enjoyable experience. With well-trained staff
+                                        members monitoring the trampoline area, you can have peace of mind knowing that
+                                        safety is our top priority.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button"
+                                        class="button-reverse-small d-flex align-items-center gap-2"
+                                        data-bs-dismiss="modal">Close Modal</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -140,11 +191,37 @@
                             <h6 style="margin-bottom: 4px;">Variety of Attractions</h6>
                             <p class="paragraph-small" style="margin-bottom: 16px;">We offer a wide range of
                                 attractions to keep you entertained and engaged throughout your visit.</p>
-                            <button class="card-link d-flex align-items-center gap-2 p-0">
+                            <button class="card-link d-flex align-items-center gap-2 p-0" data-bs-toggle="modal"
+                                data-bs-target="#adventageModal3">
                                 More Detail
                                 <img src="{{ asset('assets/img/icon/link-arrow-icon.svg') }}" class="img-fluid"
                                     alt="Link Arrow Icon">
                             </button>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="adventageModal3" tabindex="-1"
+                        aria-labelledby="adventageModal3Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="pricing-price">Variety of Attractions</h4>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="paragraph-small">JumpZone is more than just trampolines. We offer a wide
+                                        range of attractions to keep you entertained and engaged throughout your visit.
+                                        Dive into our foam pits, challenge yourself on our ninja course, test your
+                                        skills on the slam dunk trampoline, or engage in friendly dodgeball battles.
+                                        There's something for everyone at JumpZone.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button"
+                                        class="button-reverse-small d-flex align-items-center gap-2"
+                                        data-bs-dismiss="modal">Close Modal</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -158,11 +235,37 @@
                             <h6 style="margin-bottom: 4px;">Fitness and Fun Combined</h6>
                             <p class="paragraph-small" style="margin-bottom: 16px;">Jumping on trampolines isn't just
                                 fun it's also a fantastic way to stay active and fit.</p>
-                            <button class="card-link d-flex align-items-center gap-2 p-0">
+                            <button class="card-link d-flex align-items-center gap-2 p-0" data-bs-toggle="modal"
+                                data-bs-target="#adventageModal4">
                                 More Detail
                                 <img src="{{ asset('assets/img/icon/link-arrow-icon.svg') }}" class="img-fluid"
                                     alt="Link Arrow Icon">
                             </button>
+                        </div>
+                    </div>
+
+                    <div class="modal fade" id="adventageModal4" tabindex="-1"
+                        aria-labelledby="adventageModal4Label" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="pricing-price">Fitness and Fun Combined</h4>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="paragraph-small">Jumping on trampolines isn't just fun—it's also a
+                                        fantastic way to stay active and fit. JumpZone provides a dynamic and engaging
+                                        workout that improves cardiovascular health, enhances coordination and balance,
+                                        and strengthens muscles. Get your heart pumping and experience the joy of
+                                        exercise while having a blast in our trampoline area.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button"
+                                        class="button-reverse-small d-flex align-items-center gap-2"
+                                        data-bs-dismiss="modal">Close Modal</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
