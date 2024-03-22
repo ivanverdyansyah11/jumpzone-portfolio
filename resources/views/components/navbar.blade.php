b006db29e8c950d0a8291e016b0eef162dd944f3
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 23:15:06 +0800

    Progress: Add New Component Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,23 @@
+{{-- NAVBAR --}}
+<nav class="navbar navbar-expand-lg py-3 position-relative" data-aos-once="true" data-aos="fade-down"
+    data-aos-duration="1400">
+    <div class="container d-flex justify-content-between">
+        <a class="navbar-brand" href="#home">
+            <img src="{{ asset('assets/img/logo/logo-brand.svg') }}" alt="Logo Brand" width="175">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-lg-flex justify-content-lg-center" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link py-2 py-lg-0 px-lg-3 active" href="#home">Home</a>
+                <a class="nav-link py-2 py-lg-0 px-lg-3" href="#about">About Us</a>
+                <a class="nav-link py-2 py-lg-0 px-lg-3" href="#pricing">Pricing</a>
+            </div>
+        </div>
+        <a href="https://wa.me/6281234567890" target="_blank"
+            class="button-reverse-small d-none d-lg-inline-block">Contact Us</a>
+    </div>
+</nav>
+{{-- END NAVBAR --}}
